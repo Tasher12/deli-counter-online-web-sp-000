1ff2095e9f62e9ee329ef469404028df4bfcbed5
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-01-19 17:23:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,10 @@ deli_name = []
 def take_a_number(deli_name, name)
   deli_name << name
   puts "Welcome, #{name}. You are number #{deli_name.length} in line."
+<<<<<<< HEAD
+=======
+  return deli_name
+>>>>>>> fa1db9de75103e79c0671255cd70d1cc0c577b74
 end
 
 
@@ -15,6 +19,7 @@ def now_serving(deli_name)
   end
 end
 
+<<<<<<< HEAD
 def line(deli_name)
   if deli_name.count > 1
    output = "The line is currently:"
@@ -27,3 +32,15 @@ end
 
 
 
+=======
+
+def line(deli_name)
+  names = []
+  while deli_name.length == 0 
+    puts "The line is currently empty."
+  while deli_name != 0  
+    names << deli_name.each.with_index do |name, index|;
+      puts "The line is currently: #{index.to_i + 1}. #{name}"
+  end 
+end
+>>>>>>> fa1db9de75103e79c0671255cd70d1cc0c577b74
